1880a35ea4e8e13bf87be9239a8859107348dc23
budpoetra <budpoetra@gmail.com>
2022-07-11 21:24:07 +0700

    Inheritance

--- produk.php
+++ produk.php
@@ -5,19 +5,45 @@ class Produk
     public $judul,
         $penulis,
         $penerbit,
-        $harga;
+        $harga,
+        $prop;
 
-    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0)
+    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $prop = 0, $waktuMain = 0)
     {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
+        $this->prop = $prop;
     }
 
     public function getLabel()
     {
         return "$this->penulis, $this->penerbit.";
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        return $str;
+    }
+}
+
+class Komik extends Produk
+{
+    public function getInfoProduk()
+    {
+        $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->prop} Halaman.";
+        return $str;
+    }
+}
+
+class Game extends Produk
+{
+    public function getInfoProduk()
+    {
+        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ~ {$this->prop} Jam.";
+        return $str;
     }
 }
 
@@ -30,13 +56,10 @@ class infoProduk
     }
 }
 
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
 
-$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
 
-echo "Komit : " . $produk1->getLabel();
+echo $produk1->getInfoProduk();
 echo "<br>";
-echo "Game : " . $produk2->getLabel();
-echo "<br>";
-$infoProduk1 = new infoProduk();
-echo $infoProduk1->cetak($produk2);
+echo $produk2->getInfoProduk();
