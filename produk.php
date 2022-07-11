26962bf46a51a5880d714806542a41f5d2e94726
budpoetra <budpoetra@gmail.com>
2022-07-11 22:37:08 +0700

    Setter & Getter

--- produk.php
+++ produk.php
@@ -2,13 +2,11 @@
 
 class Produk
 {
-    public $judul,
+    private $judul,
         $penulis,
-        $penerbit;
-
-    protected $diskon;
-
-    private $harga;
+        $penerbit,
+        $harga,
+        $diskon;
 
     public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0)
     {
@@ -29,9 +27,63 @@ class Produk
         return $str;
     }
 
+    public function setDiskon($diskon = 0)
+    {
+        $this->diskon = $diskon;
+    }
+
+    public function getDiskon()
+    {
+        return $this->diskon;
+    }
+
     public function getHarga()
     {
         return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+
+    public function setHarga($harga)
+    {
+        $this->harga = $harga;
+    }
+
+    public function getJudul()
+    {
+        return $this->judul;
+    }
+
+    public function setJudul($judul)
+    {
+        if (!is_string($judul)) {
+            throw new Exception("Harus String");
+        }
+        $this->judul = $judul;
+    }
+
+    public function getPenulis()
+    {
+        return $this->penulis;
+    }
+
+    public function setPenulis($penulis)
+    {
+        if (!is_string($penulis)) {
+            throw new Exception("Harus String");
+        }
+        $this->penulis = $penulis;
+    }
+
+    public function getPenerbit()
+    {
+        return $this->penerbit;
+    }
+
+    public function setPenerbit($penerbit)
+    {
+        if (!is_string($penerbit)) {
+            throw new Exception("Harus String");
+        }
+        $this->penerbit = $penerbit;
     }
 }
 
@@ -69,11 +121,6 @@ class Game extends Produk
         $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.";
         return $str;
     }
-
-    public function setDiskon($diskon = 0)
-    {
-        $this->diskon = $diskon;
-    }
 }
 
 $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
@@ -87,3 +134,11 @@ echo "<hr>";
 
 $produk2->setDiskon(10);
 echo $produk2->getHarga();
+echo "<hr>";
+
+$produk3 = new Produk("BarangBaru");
+echo $produk3->getJudul();
+echo "<br>";
+$produk3->setJudul("123");
+echo $produk3->getJudul();
+echo $produk3->getPenulis();
