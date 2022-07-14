51ee73a214e4a068af2da569c53ae0d0e1af46e6
budpoetra <budpoetra@gmail.com>
2022-07-14 22:05:02 +0700

    Abstract Class

--- produk.php
+++ produk.php
@@ -1,6 +1,6 @@
 <?php
 
-class Produk
+abstract class Produk
 {
     private $judul,
         $penulis,
@@ -21,7 +21,9 @@ class Produk
         return "$this->penulis, $this->penerbit.";
     }
 
-    public function getInfoProduk()
+    abstract public function getInfoProduk();
+
+    public function getInfo()
     {
         $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
         return $str;
@@ -100,7 +102,7 @@ class Komik extends Produk
 
     public function getInfoProduk()
     {
-        $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
+        $str = "Komik : " . $this->getInfo() . " - {$this->jmlHalaman} Halaman.";
         return $str;
     }
 }
@@ -118,7 +120,25 @@ class Game extends Produk
 
     public function getInfoProduk()
     {
-        $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.";
+        $str = "Game : " . $this->getInfo() . " ~ {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+class CetakInfoProduk {
+    public $daftarProduk = array();
+
+    public function tambahProduk(Produk $produk)
+    {
+        $this->daftarProduk[] = $produk;
+    }
+
+    public function cetak()
+    {
+        $str = "DAFTAR PRODUK : <br>";
+        foreach($this->daftarProduk as $p) {
+            $str .= "- {$p->getInfoProduk()} <br>";
+        }
         return $str;
     }
 }
@@ -127,18 +147,7 @@ $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
 
 $produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
 
-echo $produk1->getInfoProduk();
-echo "<br>";
-echo $produk2->getInfoProduk();
-echo "<hr>";
-
-$produk2->setDiskon(10);
-echo $produk2->getHarga();
-echo "<hr>";
-
-$produk3 = new Produk("BarangBaru");
-echo $produk3->getJudul();
-echo "<br>";
-$produk3->setJudul("123");
-echo $produk3->getJudul();
-echo $produk3->getPenulis();
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
